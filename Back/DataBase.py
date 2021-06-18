f6adc530ad95373693f4325f4e5d95d16a1a35d2
RacheAndolini <59520565+RacheAndolini@users.noreply.github.com>
2021-06-18 15:02:02 +0800

    fix bugs

--- Back/DataBase.py
+++ Back/DataBase.py
@@ -89,14 +89,18 @@ class DataBase():
             sql = "INSERT INTO Users VALUES(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',%d,\'%s\',\'%s\')" % (U_ID,Phone,Pass,U_Name,Gender,Age,U_Identity,U_Profile)
         #print(U_ID)
         #print(sql)
-        self.cursor.execute(sql)
-        if U_Identity=='D':
-            sql = "INSERT INTO Doctors VALUES(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')" % (U_ID,Certificate_ID,Title,Department,WorkTime)
+        try:
             self.cursor.execute(sql)
-        #else:
-            #sql = "INSERT INTO MR_Records VALUES(\'%s\',\'%s\',\'0000000000\',CONVERT(smalldatetime,\'0000-00-00 00:00:00\',20),\'\',\'\',NULL" % (U_ID+'00000',U_ID)
-            #self.cursor.execute(sql) 
-        self.conn.commit()
+            if U_Identity=='D':
+                sql = "INSERT INTO Doctors VALUES(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')" % (U_ID,Certificate_ID,Title,Department,WorkTime)
+                self.cursor.execute(sql)
+            #else:
+                #sql = "INSERT INTO MR_Records VALUES(\'%s\',\'%s\',\'0000000000\',CONVERT(smalldatetime,\'0000-00-00 00:00:00\',20),\'\',\'\',NULL" % (U_ID+'00000',U_ID)
+                #self.cursor.execute(sql) 
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
         
         return 0,U_ID
 
@@ -111,7 +115,7 @@ class DataBase():
         '''
         Ap_Time format: yyyy-mm-dd hh:mm:ss
         '''
-        sql = "INSERT INTO Appointments VALUES(\'%s\',\'%s\',CONVERT(smalldatetime,\'%s\',20),\'%s\',\'%s\'" % (Patient_ID,Doctor_ID,Ap_Time,Description,Location)
+        sql = "INSERT INTO Appointments VALUES(\'%s\',\'%s\',CONVERT(smalldatetime,\'%s\',20),\'%s\',\'%s\')" % (Patient_ID,Doctor_ID,Ap_Time,Description,Location)
         try:
             self.cursor.execute(sql)
             self.conn.commit()
